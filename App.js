fa13cc19ac2e12ff3fb856931af3904bac67ce7c
Melanie-Samedi <melanie.samedi@upsell.fr>
2018-02-28 16:57:15 +0100

    send a notif when app is in backround

--- App.js
+++ App.js
@@ -1,9 +1,49 @@
-import React from 'react';
-import { StackNavigator } from 'react-navigation';
-import HomeScreen from './pages/HomeScreen';
-import Article from './pages/Article';
+import React from "react";
+import { StackNavigator } from "react-navigation";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  TouchableOpacity,
+  AppRegistry,
+  AppState,
+} from "react-native";
+import PushController from "./components/common/PushController";
+import PushNotification from "react-native-push-notification";
 
-const App = StackNavigator(
+import HomeScreen from "./pages/HomeScreen";
+import Article from "./pages/Article";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  componentDidMount() {
+    AppState.addEventListener("change", this.handleAppStateChange);
+  }
+  componentWillUnmount() {
+    AppState.removeEventListener("change", this.handleAppStateChange);
+  }
+  handleAppStateChange = () => {
+    if (AppState.currentState === "background") {
+      PushNotification.localNotification({
+        message: "Checkout the news !",
+      });
+    }
+  };
+
+  render() {
+    return (
+      <View style={styles.mainBackground}>
+        <MyScreens />
+        <PushController />
+      </View>
+    );
+  }
+}
+
+const MyScreens = StackNavigator(
   {
     Home: { screen: HomeScreen },
     Article: { screen: Article },
@@ -11,8 +51,58 @@ const App = StackNavigator(
   {
     navigationOptions: {
       header: null,
-    }
-  }
+    },
+  },
 );
 
-export default App;
+const styles = StyleSheet.create({
+  mainBackground: {
+    flex: 1,
+    backgroundColor: "#FFF",
+    flexDirection: "column",
+    justifyContent: "flex-end",
+  },
+  fromWrapper: {},
+  form: {
+    paddingHorizontal: 30,
+  },
+  label: {
+    fontSize: 11,
+    color: "#000",
+    fontWeight: "500",
+    marginTop: 24,
+  },
+  input: {
+    lineHeight: 20,
+    borderBottomColor: "#F0F0F0",
+    borderBottomWidth: 1,
+    fontSize: 16,
+    paddingVertical: 11,
+  },
+  signin: {
+    height: 60,
+    backgroundColor: "#FC6535",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 8,
+    marginTop: 128,
+  },
+  textBtn: {
+    color: "#fff",
+    fontSize: 12,
+    fontWeight: "700",
+  },
+  bottomBar: {
+    backgroundColor: "#FC6535",
+    height: 6,
+  },
+  connecWrapper: {
+    flexDirection: "row",
+    justifyContent: "center",
+    marginVertical: 30,
+  },
+  textConnec: {
+    fontSize: 12,
+    fontWeight: "500",
+  },
+});
--- components/common/PushController.js
+++ components/common/PushController.js
@@ -4,13 +4,9 @@ import PushNotification from "react-native-push-notification";
 export default class PushController extends Component {
   componentDidMount() {
     PushNotification.configure({
+      // (required) Called when a remote or local notification is opened or received
       onNotification: function(notification) {
         console.log("NOTIFICATION:", notification);
-
-        // process the notification
-
-        // required on iOS only (see fetchCompletionHandler docs: https://facebook.github.io/react-native/docs/pushnotificationios.html)
-        notification.finish(PushNotificationIOS.FetchResult.NoData);
       },
     });
   }
