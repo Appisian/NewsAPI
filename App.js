d4c99b2e466e7e9a05baafb751c1c42f6051bda6
Appisian <tangthanhbinh77@gmail.com>
2018-02-28 15:48:17 +0100

    Create a default App class

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, AppRegistry } from 'react-native';
+
 import HomeScreen from './pages/HomeScreen';
 import Article from './pages/Article';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
-const App = StackNavigator(
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.mainBackground}>
+        <MyScreens />
+      </View>
+    );
+  }
+}
+
+const MyScreens = StackNavigator(
   {
     Home: { screen: HomeScreen },
     Article: { screen: Article },
@@ -15,4 +29,55 @@ const App = StackNavigator(
   }
 );
 
-export default App;
+const styles = StyleSheet.create({
+  mainBackground: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  fromWrapper: {
+  },
+  form: {
+    paddingHorizontal: 30,
+  },
+  label: {
+    fontSize: 11,
+    color: '#000',
+    fontWeight: '500',
+    marginTop: 24,
+  },
+  input: {
+    lineHeight: 20,
+    borderBottomColor: '#F0F0F0',
+    borderBottomWidth: 1,
+    fontSize: 16,
+    paddingVertical: 11,
+  },
+  signin: {
+    height: 60,
+    backgroundColor: '#FC6535',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    marginTop: 128,
+  },
+  textBtn: {
+    color: '#fff',
+    fontSize: 12, 
+    fontWeight: '700',
+  },
+  bottomBar: {
+    backgroundColor: '#FC6535',
+    height: 6,
+  },
+  connecWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+  },
+  textConnec: {
+    fontSize: 12,
+    fontWeight: '500',
+  }
+})
