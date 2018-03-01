d10e60b2cfebf20e333597b8c5537f2e5045b482
Appisian <tangthanhbinh77@gmail.com>
2018-03-01 01:13:05 +0100

    Sign In and Sign Up pages connect to Firebase & display error on submit

---
+++ pages/SignIn.js
@@ -0,0 +1,153 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+import firebase from 'firebase';
+
+// import Spinner from './components/common/Spinner';
+
+export default class SignIn extends React.Component {
+
+  static navigationOptions = {
+    title: 'Sign In',
+  };  
+
+  constructor() {
+    super();
+    
+    this.state = {
+      email: '',
+      password: '',
+      error: '',
+      loading: false,
+    }
+  }
+
+  _onLoginPress = () => {
+    this.setState({ error: '', loading: true });
+
+    const { email, password } = this.state;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ error: '', loading: false });
+        this.props.navigation.navigate('ArticleList')
+      })
+      .catch((e) => {
+        this.setState({ error: 'Authentication failed.', loading: false});
+        Alert.alert(
+          this.state.error,
+          e.message,
+          [
+            {text: 'OK'},
+          ],
+          { cancelable: false }
+        )
+      })
+  }
+
+  _renderButtonOrSpinner = () => {
+    if(this.state.loading) {
+      // return <Spinner />
+    }
+    return <Button onPress={this._onLoginPress.bind(this)} title="Log in" />
+  }
+
+  render() {
+    const { navigate, goBack } = this.props.navigation;
+
+    return (
+      <View style={styles.mainBackground}>
+        <View style={styles.fromWrapper}>
+          <View style={styles.form}>
+            <Text style={styles.label}>EMAIL</Text>
+            <TextInput 
+              style={styles.input} 
+              keyboardType={"email-address"}
+              placeholder="stephen.smith@gmail.com" 
+              placeholderTextColor="#000"
+              onChangeText={email => this.setState({ email })}
+            />
+            <Text style={styles.label}>PASSWORD</Text>
+            <TextInput 
+              style={styles.input} 
+              placeholder="•••••••" 
+              placeholderTextColor="#000" 
+              secureTextEntry={true}
+              onChangeText={password => this.setState({ password })}
+            ></TextInput>
+
+            <TouchableOpacity
+              onPress={this._onLoginPress}
+            >
+              <View style={styles.signin}>
+                <Text style={styles.textBtn}>SIGN IN</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={() => navigate('SignUp')}
+            >
+              <View style={styles.connecWrapper}>
+                <Text style={styles.textConnec}>Don't have an account ?</Text>
+                <Text style={[styles.textConnec, {fontWeight: '700'}]}> Sign up</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+        </View>
+        <View style={styles.bottomBar}></View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainBackground: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  fromWrapper: {
+  },
+  form: {
+    paddingHorizontal: 30,
+  },
+  label: {
+    fontSize: 11,
+    color: '#000',
+    fontWeight: '500',
+    marginTop: 24,
+  },
+  input: {
+    lineHeight: 20,
+    borderBottomColor: '#F0F0F0',
+    borderBottomWidth: 1,
+    fontSize: 16,
+    paddingVertical: 11,
+  },
+  signin: {
+    height: 60,
+    backgroundColor: '#FC6535',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    marginTop: 128,
+  },
+  textBtn: {
+    color: '#fff',
+    fontSize: 12, 
+    fontWeight: '700',
+  },
+  bottomBar: {
+    backgroundColor: '#FC6535',
+    height: 6,
+  },
+  connecWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+  },
+  textConnec: {
+    fontSize: 12,
+    fontWeight: '500',
+  }
+})
---
+++ pages/SignUp.js
@@ -0,0 +1,152 @@
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity, Alert } from 'react-native';
+import firebase from 'firebase';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+export default class SignUp extends React.Component {
+  
+  
+
+  static navigationOptions = {
+    title: 'Sign Up',
+  };  
+
+  constructor(props) {
+    super(props);
+    this.state = { email: '', password: '', error: '', loading: false };
+  }
+
+  _onLoginPress = () => {
+    console.warn('In proccess')
+    this.setState({ error: '', loading: true });
+
+    const { email, password } = this.state;
+    firebase.auth().createUserWithEmailAndPassword(email, password)
+      .then(() => {
+        this.setState({ error: '', loading: false });
+        this.props.navigation.navigate('ArticleList');
+      })
+      .catch((e) => {
+        this.setState({ error: 'Sign up failed.', loading: false});
+        Alert.alert(
+          this.state.error,
+          e.message,
+          [
+            {text: 'OK'},
+          ],
+          { cancelable: false }
+        )
+      });
+  }
+
+  render() {
+    const { navigate, goBack } = this.props.navigation;
+
+    return (
+      <View style={styles.mainBackground}>
+        <View style={styles.backWrapper}>
+          <TouchableOpacity onPress={() => goBack()}><Icon style={styles.icons} name="arrow-back" size={24} /></TouchableOpacity>
+        </View>
+        <View style={styles.fromWrapper}>
+          <View style={styles.form}>
+            <Text style={styles.label}>NAME</Text>
+            <TextInput 
+              style={styles.input} 
+              placeholder="Stephen Smith" 
+              placeholderTextColor="#000"
+            ></TextInput>
+            <Text style={styles.label}>EMAIL</Text>
+            <TextInput 
+              style={styles.input} 
+              keyboardType={"email-address"} 
+              placeholder="stephen.smith@gmail.com" 
+              placeholderTextColor="#000"
+              onChangeText={email => this.setState({ email })}  
+            ></TextInput>
+            <Text style={styles.label}>PASSWORD</Text>
+            <TextInput 
+              style={styles.input} 
+              placeholder="•••••••" 
+              placeholderTextColor="#000" 
+              secureTextEntry={true}
+              onChangeText={password => this.setState({ password })}
+            ></TextInput>
+
+            <TouchableOpacity onPress={this._onLoginPress}>
+              <View style={styles.signup}>
+                <Text style={styles.textBtn}>SIGN UP</Text>
+              </View>
+            </TouchableOpacity>
+
+            <TouchableOpacity>
+              <View style={styles.connecWrapper}>
+                <Text style={styles.textConnec}>Already have an account ?</Text>
+                <Text style={[styles.textConnec, {fontWeight: '700'}]}> Sign in</Text>
+              </View>
+            </TouchableOpacity>
+
+          </View>
+        </View>
+        <View style={styles.bottomBar}></View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  mainBackground: {
+    flex: 1,
+    backgroundColor: '#FFF',
+    flexDirection: 'column',
+    justifyContent: 'flex-end',
+  },
+  fromWrapper: {
+  },
+  form: {
+    paddingHorizontal: 30,
+  },
+  label: {
+    fontSize: 11,
+    color: '#000',
+    fontWeight: '500',
+    marginTop: 24,
+  },
+  input: {
+    lineHeight: 20,
+    borderBottomColor: '#F0F0F0',
+    borderBottomWidth: 1,
+    fontSize: 16,
+    paddingVertical: 11,
+  },
+  signup: {
+    height: 60,
+    backgroundColor: '#FC6535',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 8,
+    marginTop: 88,
+  },
+  textBtn: {
+    color: '#fff',
+    fontSize: 12, 
+    fontWeight: '700',
+  },
+  bottomBar: {
+    backgroundColor: '#FC6535',
+    height: 6,
+  },
+  connecWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginVertical: 30,
+  },
+  textConnec: {
+    fontSize: 12,
+    fontWeight: '500',
+  },
+  backWrapper: {
+    position: 'absolute',
+    top: 40,
+    left: 10,
+  }
+})
