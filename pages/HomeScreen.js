5f2dc6e8fefa07cf5bb86338154aeb61e3b7e041
Appisian <tangthanhbinh77@gmail.com>
2018-03-01 01:13:39 +0100

    Adding TabNavigation between Top Headlines and Latest news

--- pages/HomeScreen.js
+++ pages/HomeScreen.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View, AppRegistry } from 'react-native';
+import { TabNavigator, TabBarBottom } from 'react-navigation';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
 import Header from '../components/common/Header';
 import ArticleList from '../components/common/ArticleList';
 
-export default class HomeScreen extends React.Component {
+class Everything extends React.Component {
 
   static navigationOptions = {
-    title: 'Home',
+    title: 'Latest News',
     headerStyle: {
       backgroundColor: '#212121',
     },
@@ -26,7 +29,37 @@ export default class HomeScreen extends React.Component {
       <View style={styles.mainBackground}>
         <Header title="LATEST NEWS" navigate={navigate} goBack={goBack} displaySearch={true}></Header>
         <View style={styles.wrapper}>
-          <ArticleList navigate={navigate} goBack={goBack}></ArticleList> 
+          <ArticleList navigate={navigate} goBack={goBack} req={"https://newsapi.org/v2/everything?sources=google-news&apiKey=025f0aa223a443ce8b1ee55f41bff8a9&pageSize=10&page=${page}"}></ArticleList> 
+        </View>
+      </View>
+    );
+  }
+}
+
+class TopHeadlines extends React.Component {
+
+  static navigationOptions = {
+    title: 'Top Headlines',
+    headerStyle: {
+      backgroundColor: '#212121',
+    },
+    headerTitleStyle: {
+      color: '#fff',
+    }
+  };  
+
+  constructor() {
+    super();
+  }
+
+  render() {
+    const { navigate, goBack } = this.props.navigation;
+
+    return (
+      <View style={styles.mainBackground}>
+        <Header title="TOP HEADLINES" navigate={navigate} goBack={goBack} displaySearch={true}></Header>
+        <View style={styles.wrapper}>
+          <ArticleList navigate={navigate} goBack={goBack} req={"https://newsapi.org/v2/top-headlines?country=us&apiKey=025f0aa223a443ce8b1ee55f41bff8a9"}></ArticleList> 
         </View>
       </View>
     );
@@ -45,3 +78,29 @@ const styles = StyleSheet.create({
     overflow: 'visible',
   }
 })
+
+export default TabNavigator({
+  Home: { screen: TopHeadlines },
+  Everything: { screen: Everything },
+}, {
+  navigationOptions: ({navigation}) => ({
+    tabBarIcon: ({focused, tintColor}) => {
+      const { routeName } = navigation.state;
+      let iconName;
+      if( routeName === 'Home' ) {
+        iconName = 'new-releases';
+      } else if( routeName === 'Everything') {
+        iconName = 'subject';
+      }
+      return <Icon name={iconName} size={25} color={tintColor} />;
+    }
+  }),
+  tabBarOptions: {
+    activeTintColor: '#FC6535',
+    inactiveTintColor: 'gray',
+  },
+  tabBarComponent: TabBarBottom,
+  tabBarPosition: 'bottom',
+  animationEnabled: true,
+  swipeEnabled: true,
+});
